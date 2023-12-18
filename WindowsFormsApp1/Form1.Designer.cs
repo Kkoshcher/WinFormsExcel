4a2185a0a936d4637413f503583712aae2c79b10
Kkoshcher <tn36m29rc1a7@gmail.com>
2023-12-18 17:34:01 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.Selbtn = new System.Windows.Forms.Button();
+            this.btnre = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoad.Location = new System.Drawing.Point(22, 33);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(147, 48);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "載入檔案";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1071, 475);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(250, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 36);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(463, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(193, 36);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(681, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(193, 36);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(890, 75);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(193, 36);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownload.Location = new System.Drawing.Point(1114, 544);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(102, 48);
+            this.btnDownload.TabIndex = 6;
+            this.btnDownload.Text = "輸出";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(250, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 32);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(463, 33);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(138, 32);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(681, 33);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(138, 32);
+            this.comboBox3.TabIndex = 9;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(890, 33);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(138, 32);
+            this.comboBox4.TabIndex = 10;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // Selbtn
+            // 
+            this.Selbtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Selbtn.Location = new System.Drawing.Point(1114, 370);
+            this.Selbtn.Name = "Selbtn";
+            this.Selbtn.Size = new System.Drawing.Size(102, 48);
+            this.Selbtn.TabIndex = 11;
+            this.Selbtn.Text = "篩選";
+            this.Selbtn.UseVisualStyleBackColor = true;
+            this.Selbtn.Click += new System.EventHandler(this.Selbtn_Click);
+            // 
+            // btnre
+            // 
+            this.btnre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnre.Location = new System.Drawing.Point(1114, 424);
+            this.btnre.Name = "btnre";
+            this.btnre.Size = new System.Drawing.Size(102, 48);
+            this.btnre.TabIndex = 12;
+            this.btnre.Text = "還原";
+            this.btnre.UseVisualStyleBackColor = true;
+            this.btnre.Click += new System.EventHandler(this.btnre_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1228, 623);
+            this.Controls.Add(this.btnre);
+            this.Controls.Add(this.Selbtn);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnLoad);
+            this.Font = new System.Drawing.Font("新細明體", 18F);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "數據篩選";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button Selbtn;
+        private System.Windows.Forms.Button btnre;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            comboBox2.Visible = false;
+            textBox2.Visible = false;
+            comboBox3.Visible = false;
+            textBox3.Visible = false;
+            comboBox4.Visible = false;
+            textBox4.Visible = false;
+        }
+        private string FormatCellValue(IXLCell cell) {
+            if (cell.DataType == XLDataType.DateTime) {
+                return cell.GetDateTime().ToString("yyyy/MM/dd");
+            }
+            return cell.Value.ToString();
+        }
+        private DataTable originalDataTable;
+        private void btnLoad_Click(object sender, EventArgs e) {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                string filePath = openFileDialog.FileName;
+                using (var workbook = new XLWorkbook(filePath)) {
+                    var worksheet = workbook.Worksheet(1);
+                    DataTable dt = new DataTable();
+
+                    // 清除舊的 ComboBox 項目
+                    RepComBtn();
+
+                    // 添加列標題到 DataTable 和 ComboBox
+                    var firstRow = worksheet.FirstRowUsed();
+                    int columnCount = firstRow.CellsUsed().Count(); 
+                    foreach (var cell in firstRow.Cells(1, columnCount)) { 
+                        string columnName = cell.Value.ToString();
+                        dt.Columns.Add(columnName);
+                        comboBox1.Items.Add(columnName);
+                        comboBox2.Items.Add(columnName);
+                        comboBox3.Items.Add(columnName);
+                        comboBox4.Items.Add(columnName);
+                    }
+
+                    // 並添加數據
+                    var rows = worksheet.RowsUsed().Skip(1);
+                    foreach (var row in rows) {
+                        dt.Rows.Add(row.Cells(1, columnCount).Select(c => FormatCellValue(c)).ToArray()); 
+                    }
+
+                    dataGridView1.DataSource = dt;
+                    MessageBox.Show("加載操作：原始數據表行數 = " + dt.Rows.Count);
+                    originalDataTable = dt;
+                }
+            }
+
+        }
+
+        private string FormatCellValue(object value) {
+            if (value is DateTime) {
+                return ((DateTime)value).ToString("yyyy/MM/dd");
+            }
+            return value?.ToString() ?? "";
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e) {
+            DataTable dt = dataGridView1.DataSource as DataTable;
+            if (dt == null) {
+                DataView dataView = dataGridView1.DataSource as DataView;
+                if (dataView != null) {
+                    dt = dataView.ToTable();
+                }
+            }
+
+            if (dt != null) {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Excel Files|*.xlsx";
+                saveFileDialog.DefaultExt = "xlsx";
+                saveFileDialog.AddExtension = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK) {
+                    using (var workbook = new XLWorkbook()) {
+                        var worksheet = workbook.Worksheets.Add("Exported Data");
+
+                        // 添加列標題
+                        for (int i = 0; i < dt.Columns.Count; i++) {
+                            worksheet.Cell(1, i + 1).Value = dt.Columns[i].ColumnName;
+                        }
+
+                        // 添加數據
+                        for (int i = 0; i < dt.Rows.Count; i++) {
+                            for (int j = 0; j < dt.Columns.Count; j++) {
+                                worksheet.Cell(i + 2, j + 1).Value = FormatCellValue(dt.Rows[i][j]);
+                            }
+                        }
+
+                        workbook.SaveAs(saveFileDialog.FileName);
+
+                        MessageBox.Show("文件已成功保存！");
+                        this.Close(); 
+                    }
+                }
+            }
+            else {
+                MessageBox.Show("無可導出的數據");
+            }
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void Selbtn_Click(object sender, EventArgs e) {
+            if (originalDataTable != null) {
+                var dataView = originalDataTable.DefaultView;
+
+                // 構建篩選條件字符串
+                var filterConditions = new List<string>();
+                if (comboBox1.SelectedIndex != -1 && !string.IsNullOrEmpty(textBox1.Text)) {
+                    filterConditions.Add($"[{comboBox1.SelectedItem.ToString()}] LIKE '%{textBox1.Text}%'");
+                }
+                if (comboBox2.SelectedIndex != -1 && !string.IsNullOrEmpty(textBox2.Text)) {
+                    filterConditions.Add($"[{comboBox2.SelectedItem.ToString()}] LIKE '%{textBox2.Text}%'");
+                }
+                if (comboBox3.SelectedIndex != -1 && !string.IsNullOrEmpty(textBox3.Text)) {
+                    filterConditions.Add($"[{comboBox3.SelectedItem.ToString()}] LIKE '%{textBox3.Text}%'");
+                }
+                if (comboBox4.SelectedIndex != -1 && !string.IsNullOrEmpty(textBox4.Text)) {
+                    filterConditions.Add($"[{comboBox4.SelectedItem.ToString()}] LIKE '%{textBox4.Text}%'");
+                }
+
+                // 檢查第一組控件是否已被使用，如果是，則顯示第二組控件
+                if (comboBox1.SelectedIndex != -1 && !string.IsNullOrEmpty(textBox1.Text)) {
+                    comboBox2.Visible = true;
+                    textBox2.Visible = true;
+                }
+
+                // 以此類推，檢查第二組控件是否已被使用，如果是，則顯示第三組控件
+                if (comboBox2.SelectedIndex != -1 && !string.IsNullOrEmpty(textBox2.Text)) {
+                    comboBox3.Visible = true;
+                    textBox3.Visible = true;
+                }
+
+                // 類似地，檢查第三組控件
+                if (comboBox3.SelectedIndex != -1 && !string.IsNullOrEmpty(textBox3.Text)) {
+                    comboBox4.Visible = true;
+                    textBox4.Visible = true;
+                }
+
+                // 應用篩選
+                dataView.RowFilter = string.Join(" AND ", filterConditions);
+
+                // 更新 DataGridView 的數據源
+                dataGridView1.DataSource = dataView;
+            }
+        }
+
+        private void btnre_Click(object sender, EventArgs e) {
+
+            RepComBtn();
+            // 重新應用篩選
+            ApplyFilter();
+        }
+        private void RepComBtn() {
+            // 清空 ComboBox 和 TextBox
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+            comboBox3.SelectedIndex = -1;
+            comboBox4.SelectedIndex = -1;
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+            comboBox2.Visible = false;
+            textBox2.Visible = false;
+            comboBox3.Visible = false;
+            textBox3.Visible = false;
+            comboBox4.Visible = false;
+            textBox4.Visible = false;
+        }
+        private void ApplyFilter() {
+            if (originalDataTable != null) {
+                var dataView = originalDataTable.DefaultView;
+
+                dataView.RowFilter = "";
+
+                dataGridView1.DataSource = dataView;
+            }
+        }
+    }
+}
